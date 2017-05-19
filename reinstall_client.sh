ebb70e752621264ec493917dad2c1d31318958bc
tylerthome <tyler_thome@yahoo.com>
2017-05-19 01:40:47 -0700

    Update reinstall_client.sh

--- reinstall_client.sh
+++ reinstall_client.sh
@@ -4,14 +4,14 @@
 sudo echo "Root permission obtained..."
 PROJECT_ROOT_DIR=$(pwd)
 cd ROSCamera/build-desktop
-/home/tyler/Qt/5.8/gcc_64/bin/qmake ..
+$HOME/Qt/5.8/gcc_64/bin/qmake ..
 make
 sudo make install
 
 cd $PROJECT_ROOT_DIR
 mkdir build
 cd build
-/home/tyler/Qt/5.8/gcc_64/bin/qmake ../src
+$HOME/Qt/5.8/gcc_64/bin/qmake ../src
 make
 cd $PROJECT_ROOT_DIR
 echo "Ground System client build complete. Binary executable located in 'build' directory."
