466057cad78b9acc6bf82be11c7268aa164fafb0
Tyler Thome <tyler_thome@yahoo.com>
2017-05-20 13:18:54 -0700

    merge older scripts

---
+++ install_qt.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+INITIAL_DIR=$(pwd)
+cd $HOME/Downloads
+wget http://download.qt.io/official_releases/online_installers/qt-unified-linux-x64-online.run
+chmod +x qt-unified-linux-x64-online.run
+./qt-unified-linux-x64-online.run
+cd $INITIAL_DIR
+
